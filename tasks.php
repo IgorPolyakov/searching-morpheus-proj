6b710f16e31919371638d9c4cc0c2848a8f77322
Evgenii Sopov <mrseakg@gmail.com>
2018-03-20 00:29:48 +0700

    Added 7 task by MJ

--- tasks.php
+++ tasks.php
@@ -59,5 +59,33 @@ $tasks[] = array(
 	'id' => '07_kill',
 	'title' => 'Убить Вас — наслаждение, мистер Андерсон',
 	'image' => 'images/task7.png',
-	'flag' => '',
+	'description' => '
+<pre>
+;nasm 2.11.08
+section .data
+data: 	db 65h, 6bh, 60h, 66h, 39h, 6dh, 60h, 72h, 6ch, 68h, 72h, 64h, 60h, 72h, 64h, 10
+dataLen:  equ $-data        	 
+section .text
+global _start
+_start:
+xor ecx, ecx
+mov ecx, 0eh
+L1:
+mov ah, [data+ecx]
+inc ah
+mov [data+ecx], ah
+loop L1
+mov ah, [data]
+inc ah
+mov [data], ah    
+mov eax,4
+mov ebx,1
+mov ecx,data
+mov edx,dataLen
+int 80h        	 
+mov eax,1
+mov ebx,0
+int 80h;
+</pre>',
+	'flag' => 'flag:nasmisease',
 );
