e72e16984d5b08918e1a978391ff4086844a0459
IgorPolyakov <igorpolyakov@protonmail.com>
2018-03-19 15:45:15 +0700

    update title task

--- tasks.php
+++ tasks.php
@@ -5,7 +5,7 @@ $tasks = array();
 
 $tasks[] = array(
 	'id' => 1,
-	'title' => 'Дама в красном',
+	'title' => 'Ты меня слушаешь, Нео?',
 	'description' => 'Прочитайте код из матрицы',
 	'image' => 'images/task1.png',
 	'flag' => 'flag:wakeupneo',
@@ -14,7 +14,7 @@ $tasks[] = array(
 
 $tasks[] = array(
 	'id' => 2,
-	'title' => 'Гоу в матрицу',
+	'title' => 'Какой толк от телефона, если сейчас вы немы?',
 	'description' => 'Определите номер из зашифрованного сигнала<br>
 		(Внимание 5 кратный роутинг)<br>
 		<div class="form-group">
@@ -28,7 +28,7 @@ VlhwT2QxSnJNSGRPVkZwWVZrWktVVlpyVmtabFJsSnpWV3h3VVZWVU1Eaz0=</textarea>
 
 $tasks[] = array(
 	'id' => 3,
-	'title' => 'Тык тык фы12',
+	'title' => 'Тук-тук, Нео',
 	'description' => 'У вас уже есть ответ на этот вопрос. Что вам известно о дебаге в вашем браузере?',
 	'image' => 'images/task3.png',
 	'flag' => 'flag:+1776890556',
@@ -36,28 +36,28 @@ $tasks[] = array(
 
 $tasks[] = array(
 	'id' => 4,
-	'title' => 'Грубая Сила',
+	'title' => 'Проснись, Нео…',
 	'image' => 'images/task4.png',
 	'flag' => '',
 );
 
 $tasks[] = array(
 	'id' => 5,
-	'title' => 'Печенья нет!',
+	'title' => 'Не пытайся согнуть ложку; это невозможно',
 	'image' => 'images/task5.png',
 	'flag' => '',
 );
 
 $tasks[] = array(
 	'id' => 6,
-	'title' => 'Полисемия',
+	'title' => 'Добро пожаловать в реальный мир',
 	'image' => 'images/task6.png',
 	'flag' => '',
 );
 
 $tasks[] = array(
 	'id' => 7,
-	'title' => 'Выполнить',
+	'title' => 'Убить Вас — наслаждение, мистер Андерсон',
 	'image' => 'images/task7.png',
 	'flag' => '',
 );
